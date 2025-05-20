31683a57ae0fa2982600101430c5e63761a31da4
Ivan Artamonov <Vanya200373@yandex.ru>
2025-05-20 16:32:27 +0300

    Add csrf settings

--- CoffeeTimeAPI/CoffeeTimeAPI/settings.py
+++ CoffeeTimeAPI/CoffeeTimeAPI/settings.py
@@ -157,3 +157,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
